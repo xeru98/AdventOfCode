0d22c9f42f169fa77ba08a05cdf17e6fb361485f
Patrick C <xeru98@gmail.com>
2021-12-03 16:02:25 -0500

    Refactor for learning

---
+++ AdventOfCode/2021/Day2.h
@@ -0,0 +1,16 @@
+﻿//
+// Created by Xeru on 12/1/2021.
+//
+#pragma once
+
+namespace Day2 {
+	enum class Direction {
+		forward,
+		up,
+		down,
+		unknown
+	};
+	
+	void Part1();
+	void Part2();
+}
---
+++ AdventOfCode/AOCUtils.h
@@ -0,0 +1,32 @@
+﻿//
+// Created by Xeru on 11/23/2021.
+//
+#pragma once
+
+#include <string>
+#include <vector>
+#include <functional>
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <cassert>
+
+namespace AdventOfCode2021 {
+
+	template<typename T>
+	std::vector<T> LoadDataFromFile(const std::string filePath, std::function<T(std::string)> const& parse) {
+		std::vector<T> out;
+        	std::ifstream file(filePath);
+        	if (file.is_open()) {
+        		std::string line;
+        		while (std::getline(file, line)) {
+        			out.push_back(parse(line));
+        		}
+        	}
+        	return out;
+	}
+
+	std::vector<std::string> splitString(std::string searchString, std::string delimiter);
+
+}
